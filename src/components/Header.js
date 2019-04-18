919e136f069c2a9d31de36670600dd5a59b50a0c
Gillian Openshaw <gillianopenshaw@virginmedia.com>
2019-04-18 12:58:22 +0100

    Initial Set Up

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Header extends Component {
+
+    render() {
+
+        return <h1 style={styles.header}>{this.props.headerText}</h1>
+    }
+}
+
+const styles = {
+    header: {
+        color: "red"
+    }
+}
+
+export default Header;
