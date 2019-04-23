a652440f15f2131ce6f5e09e356541a63d5ab170
Gillian Openshaw <gillianopenshaw@virginmedia.com>
2019-04-23 12:51:35 +0100

    Not working now that I have tried to add an image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Globe from "./components/Globe.jpg";
 
 class Header extends Component {
     render() {
         return (
             <div>
                 <h1>My London Travel Wizard</h1>
-
+                <img src={Globe} alt="Globe" /> 
             </div>
         );
     }
